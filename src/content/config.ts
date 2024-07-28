d4bba5da652ea683d3546e27b6e3c26686caf722
iangelmanuel <iangelmanueldm@gmail.com>
2024-07-28 10:58:25 -0500

    chore: Add blog collection to site configuration

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+import { defineCollection, z } from "astro:content"
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image().refine((img) => img.width < 1200, {
+        message: "Image width must be less than 1200px"
+      }),
+
+      // Relación
+      author: z.string(),
+
+      // Relación
+      tags: z.array(z.string())
+    })
+})
+
+export const collections = {
+  blog: blogCollection
+}
